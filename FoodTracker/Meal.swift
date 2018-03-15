1d3740184b84b2676d1858130494cb0b008fbb13
jinoxst <jinoxst@gmail.com>
2018-03-15 18:10:17 +0900

    修正

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -3,7 +3,7 @@
 //  FoodTracker
 //
 //  Created by 鄭英鎮 on 2018/01/15.
-//  Copyright © 2018年 鄭英鎮. All rights reserved.
+//  Copyright © 2018年 鄭英鎮. All rights reserved. 
 //
 
 import UIKit
--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -3,7 +3,7 @@
 //  FoodTracker
 //
 //  Created by 鄭英鎮 on 2018/01/10.
-//  Copyright © 2018年 鄭英鎮. All rights reserved.
+//  Copyright © 2018年 鄭英鎮. All rights reserved. 
 //
 
 import UIKit
